9e707550951c1a4ba5d06e58562785a645aab2e0
Giampaolo Cimino <cimino@nurc.nato.int>
2016-12-15 12:45:00 +0100

    aslist can use external variable to store output  results

--- pypelines/internals/aslist.py
+++ pypelines/internals/aslist.py
@@ -1,9 +1,12 @@
 from .dag import DAGNode
 
 class AsList(DAGNode):
-    def __init__(self):
+    def __init__(self, store=None):
         super().__init__()
         self.list = []
+        self._store = store
 
     def on_data(self, data):
         self.list.append(data)
+        if not self._store is None:
+            self._store.append(data)
