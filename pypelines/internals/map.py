e8eaf877ea89b06aab9f51bf1019638904951462
Giampaolo Cimino <cimino@nurc.nato.int>
2017-03-30 12:09:20 +0200

    add propagate_none to Map

--- pypelines/internals/map.py
+++ pypelines/internals/map.py
@@ -3,13 +3,17 @@ from pickleablelambda import pickleable
 from .dag import DAGNode
 
 class Map(DAGNode):
-    def __init__(self, func):
+    def __init__(self, func, propagate_none=True):
         super().__init__()
         self._func = pickleable(func)
+        self._propagate_none = propagate_none
 
     def on_data(self, data):
         res = self._func(data)
-        self.forward_data(res)
+        if res is None and not self._propagate_none:
+            pass
+        else:
+            self.forward_data(res)
 
     def name(self):
         return super().name() #+ "; func=" + inspect.getsource(self._func)
